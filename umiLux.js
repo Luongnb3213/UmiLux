318b64e0207311cd7b02667aba1a4031fe3843d7
Luongnb3213 <luongnb3213@gmail.com>
2023-12-13 17:39:04 +0700

    update

--- umiLux.js
+++ umiLux.js
@@ -5,7 +5,7 @@ const footer_info = document.querySelectorAll(".footer_info");
 const layer = document.querySelector(".layer");
 if (logo) {
   logo.onclick = function () {
-    nav_bar.style.animation = "slidein 1s linear";
+    nav_bar.style.animation = "slidein 0.3s linear";
     nav_bar.style.display = "block";
     layer.style.display = "block";
     layer.style.opacity = "1";
@@ -24,7 +24,7 @@ if (logo) {
 Array.from(footer_info).forEach((i) => {
   i.onclick = function () {
     let footer_info_item = i.querySelector(".pc");
-    let img_dow = i.querySelector(".footer_info_head img");
+    let img_dow = i.querySelector(".footer_info_head svg");
     footer_info_item.classList.toggle("godown");
     img_dow.classList.toggle("rotate");
   };
